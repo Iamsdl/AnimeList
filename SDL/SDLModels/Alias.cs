66273b9895eb9f250221faeaaa56757c63c2f470
Iamsdl <sdl_dani@yahoo.com>
2017-12-15 14:54:58 +0200

    added alias.delete

--- SDL/SDLModels/Alias.cs
+++ SDL/SDLModels/Alias.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SDLModels
 {
@@ -26,6 +29,51 @@ namespace SDLModels
         public virtual ICollection<OVA> OVAs { get; set; }
         public virtual ICollection<Special> Specials { get; set; }
 
+        public string Delete(SDL context)
+        {
+            try
+            {
+#warning add null checks
+                if (Animes.Any())
+                {
+                    context.Animes.RemoveRange(Animes);
+                }
+                if (Books.Any())
+                {
+                    context.Books.RemoveRange(Books);
+                }
+                if (Games.Any())
+                {
+                    context.Games.RemoveRange(Games);
+                }
+                if (Mangas.Any())
+                {
+                    context.Mangas.RemoveRange(Mangas);
+                }
+                if (Movies.Any())
+                {
+                    context.Movies.RemoveRange(Movies);
+                }
+                if (ONAs.Any())
+                {
+                    context.ONAs.RemoveRange(ONAs);
+                }
+                if (OVAs.Any())
+                {
+                    context.OVAs.RemoveRange(OVAs);
+                }
+                if (Specials.Any())
+                {
+                    context.Specials.RemoveRange(Specials);
+                }
 
+                context.Aliases.Remove(this);
+                return $"Success.\nThe {Name} alias and all related entities have been deleted.";
+            }
+            catch (Exception e)
+            {
+                return "Failed.\n" + e.Message;
+            }
+        }
     }
 }
